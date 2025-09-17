04c3f9039d73a9035e1771ca8a039ba5e6463686
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-09-17 19:35:39 -0300

    Update header.php

--- view/header.php
+++ view/header.php
@@ -14,6 +14,8 @@ if (session_status() === PHP_SESSION_NONE) {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
@@ -23,22 +25,26 @@ if (session_status() === PHP_SESSION_NONE) {
                 <div class="logo">SF</div>
                 <div>
                     <div class="site-title">A Sétima Filosofia</div>
-                    <div class="muted" style="font-size:13px">Arte • Cinema • Filosofia</div>
+                    <div class="muted" style="font-size:13px; color: white">Arte • Cinema • Filosofia</div>
                 </div>
             </div>
 
             <nav>
                 <button class="menu-toggle" aria-expanded="false" id="menuToggle">Menu</button>
-                <ul id="mainNav">
-                    <li><a href="/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/index.php">Home</a></li>
-                    <li><a href="#portfolio">Categorias</a></li>
+                <ul id="mainNav" class="nav fs-6">
+                    <li class="nav-item">
+                    <a class="nav-link text-white text-decoration-none" href="/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-white text-decoration-none" href="#portfolio">Categorias</a>
+                    </li>
 
                     <?php if (isset($_SESSION['idUsuario'])): ?>
-                        <li><a href="/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/view/cadastrarTexto.php">Escrever Texto</a></li>
-                        <li><a href="/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/view/perfil.php">Perfil</a></li>
-                        <li><a href="/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/view/logout.php">Sair</a></li>
+                        <li class="nav-item"><a class="nav-link text-white text-decoration-none" href="/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/view/cadastrarTexto.php">Escrever Texto</a></li>
+                        <li class="nav-item"><a class="nav-link text-white text-decoration-none" href="/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/view/perfil.php">Perfil</a></li>
+                        <li class="nav-item"><a class="nav-link text-white text-decoration-none" href="/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/view/logout.php">Sair</a></li>
                     <?php else: ?>
-                        <li><a href="/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/view/formLogin.php">Login</a></li>
+                        <li class="nav-item"><a class="nav-link text-white text-decoration-none" href="/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/TADS6-2025-2-GWS-Carolinne-Antonio-Jaqueline/view/formLogin.php">Login</a></li>
                     <?php endif; ?>
                 </ul>
             </nav>
