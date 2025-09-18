f403eaa01926b11f28ce8e8bb67c0d0b2676fe26
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-09-17 21:38:55 -0300

    Update cadastrarTexto.php

--- view/cadastrarTexto.php
+++ view/cadastrarTexto.php
@@ -1,7 +1,10 @@
 <?php 
 include("../view/header.php");
+include("../config/conexaoBD.php");
 
 $autor = isset($_SESSION['nomeUsuario']) ? $_SESSION['nomeUsuario'] : "Anônimo";
+$queryCategorias = "SELECT idCategoria, descricaoCategoria FROM categoria";
+$resultCategorias = mysqli_query($conn, $queryCategorias);
 ?>
 
 <div class="cadastrarTexto-container">
@@ -19,6 +22,16 @@ $autor = isset($_SESSION['nomeUsuario']) ? $_SESSION['nomeUsuario'] : "Anônimo"
 
         <label for="data">Data</label>
         <input type="date" name="data" id="data" value="<?php echo date('Y-m-d'); ?>" required>
+
+        <label for="categoria" class="form-label text-light">Categoria</label>
+        <select name="categoria" id="categoria" class="form-control bg-dark text-light border-0 rounded-pill" required>
+            <option value="">Selecione uma categoria</option>
+            <?php while ($cat = mysqli_fetch_assoc($resultCategorias)) : ?>
+                <option value="<?php echo $cat['idCategoria']; ?>">
+                    <?php echo htmlspecialchars($cat['descricaoCategoria']); ?>
+                </option>
+            <?php endwhile; ?>
+        </select>
 
         <label for="autor">Autor</label>
         <input type="text" name="autor" id="autor" value="<?php echo $autor; ?>" readonly>
